6415302c62aea1e2bdcf7caff706c92987ea2239
dd-code-immersives <domilamad89@gmail.com>
2023-02-14 15:59:26 -0500

    added Blog mongoose model and updated index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const Blog = require('./model/Blogs');
+
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res) {
+
+  //query blogs 
+  try {
+    const allBlogs = await Blog.find({});
+    res.json({blogs: allBlogs });
+  }catch(e){
+    console.log(e);
+  }
 });
 
 module.exports = router;
---
+++ routes/model/Blogs.js
@@ -0,0 +1,19 @@
+//import mongoose library
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+
+//create a blogSchema 
+const blogSchema = new mongoose.Schema({
+    title: String,
+    text: String, 
+    author: String,
+    categories: [String],
+    id: {type: String, default: uuidv4()},
+    createdAt: { type: Date, default: Date.now }
+}); 
+
+//register model to collection
+const Blog = mongoose.model("sample_blog", blogSchema);
+
+//make our model accessible to outside files 
+module.exports = Blog;
