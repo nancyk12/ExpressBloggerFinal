c2dc35b614f7f0c53c4c8361ea69bcdef0ab97c8
dd-code-immersives <domilamad89@gmail.com>
2023-02-14 12:10:38 -0500

    implemented required routes

---
+++ routes/blogs.js
@@ -0,0 +1,191 @@
+const { v4: uuidv4 } = require("uuid");
+var express = require("express");
+var router = express.Router();
+
+const { db } = require("../mongo");
+
+router.get("/all", async function (req,res,next){
+
+try {     
+  await db()
+  .collection('posts')
+  .find({}) 
+  .toArray(function(err, result){
+    if (err) {
+      res.status(400).send();
+    }else {
+      res.json({
+        sucess:true,
+        posts:result
+      });
+    }
+  });
+
+} catch (error) { 
+    //500 error -> server side error 
+    res.status(500).send();
+}
+
+  //ALTERNATE WAY TO WRITE IT
+  //throw an error if fails to populate blogPosts
+  // if (blogPosts.length == 0) {
+  //   throw Error("No blog posts found");
+  // }
+  
+  // res.json({
+  //   success: true,
+  //   posts: blogPosts, 
+  // });
+});
+
+
+router.get("/get-one-example", async function (req, res, next) {
+  const blogPosts = await db()
+  .collection("posts") 
+  .findOne({
+      id: {
+        $exists: true,
+      },
+  });
+
+  console.log(blogPosts);
+
+  res.json({
+    success: true,
+    post: blogPost,
+  });
+});
+
+
+router.get("/get-one/:id", async function (req, res, next) {
+  //checking if the parameter ID was passed in 
+  if (!req.params.id) {
+    res.json({
+      success: false,
+      message: "The blog id must be provided in the url parameters",
+    });
+    return;
+  }
+
+  console.log("first");
+  //await blocks the execution until the promise resolves
+  // aka. make sure line 47 finishes before we get on 
+  // with the rest of our program 
+  const blogPosts = await db().collection("sample_blogs").findOne({
+    id: req.params.id,
+  }); /*.catch({
+    console.log("something went wrong");
+  });*/
+  console.log("second");
+  //we don't want to a user to see the result before post is loaded.
+  // so we use await, to block this from happening.
+  res.json({
+    success: true,
+    posts: blogPosts,
+  });
+  //NOTE: FIND ONE is READ operation, the output holds the results of the operation.
+  // so we add it in our res.json()
+  console.log("third");
+});
+
+router.post("/create-one", async function (req, res, next) {
+  try {
+    const newPost = {
+      id: uuidv4(),
+      createdAt: new Date(),
+      title: req.body.title,
+      text: req.body.text,
+      author: req.body.author,
+      email: req.body.email,
+      categories: req.body.categories,
+      starRating: Number(req.body.starRating),
+    };
+
+    //debug: print out newPost
+    console.log(newPost);
+
+    // Validation
+
+    if (newPost.email === undefined || !newPost.email.split("@").length > 1) {
+
+      res.json({
+        success: false,
+        message: "email invalid",
+      });
+    }
+
+    //because insertONE is a WRITE Operation, you don't need 
+    // to return insertOpREs to the user. Only do that when it's
+    // a READ operation i.e.e (find, findOne etc.. )
+    await db().collection("sample_blogs").insertOne(newPost);
+
+    res.json({
+      success: true,
+      newPost,
+    });
+  } catch (e) {
+    console.log(typeof e);
+    console.log(e);
+    res.json({
+      error: e.toString(),
+    });
+  }
+});
+
+router.get("/get-multi", async function (req, res) {
+  const sortField = req.query.sortField;
+  const sortOrder = Number(req.query.sortOrder);
+  const limit = Number(req.query.limit);
+  const page = Number(req.query.page);
+
+  console.log(sortField, typeof sortField);
+  console.log(sortOrder, typeof sortOrder);
+  console.log(limit, typeof limit);
+  console.log(page, typeof page);
+
+  const skip = limit * (page - 1);
+  console.log(skip);
+
+	const sortObject = {}
+	sortObject[sortField] = sortOrder 
+  // {title: 1} -> sort ascending 
+  // {title: -1} -> sort descending
+
+  const blogs = await db()
+  .collection("sample_blogs")
+  .find({})
+  .sort(sortObject)
+  .limit(limit)
+  .skip(skip)
+  .toArray()
+
+  res.json({
+		success: true,
+		blogs
+	});
+});
+
+router.delete('/delete-multi', async function (req, res) {
+	try {
+      
+      const idsToDelete = req.body
+
+      if (idsToDelete.length < 1){
+        throw Error("ids to delete empty!");
+      }
+      const deleteResult = await db().collection("sample_blogs").deleteMany({
+        id: {
+          $in: idsToDelete
+        }
+      })
+  
+  } catch (e) {
+    res.send(e);
+  }
+	res.json({
+		success: true,
+		deleteResult: deleteResult
+	})
+})
+
+module.exports = router;
---
+++ routes/sync_async_notes.js
@@ -0,0 +1,104 @@
+//Synchronous code:
+
+const name = 'Miriam';
+const greeting = `Hello, my name is ${name}!`;
+console.log(greeting);
+// "Hello, my name is Miriam!"
+
+/**
+ * 
+ * makeGreeting() is a synchronous function 
+ * because the caller has to wait for the function 
+ * to finish its work and return a value before the caller can continue.
+ * 
+ */
+function makeGreeting(name) {
+  return `Hello, my name is ${name}!`;
+}
+
+const name2 = 'Miriam';
+// because the program is synchronous we have to wait 
+// until the function call makeGreeting returns a result.
+const greeting2 = makeGreeting(name2); 
+//once we have the result, we print it out.
+console.log(greeting2);
+// "Hello, my name is Miriam!"
+
+
+/**
+ * 
+ * 
+ * INTRO TO PROMISES 
+ */
+
+//we use fetch to make a get request, 
+// all requests are by default asynchronous in javascript
+//promises allow us to execute code when the time is right 
+
+
+
+// ex1. ----------------------------------
+
+const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+// print out 1.
+console.log(fetchPromise);
+
+// print out 3.
+fetchPromise.then((response) => {
+  console.log(`Received response: ${response.status}`);
+});
+
+//print out 2.
+console.log("Started request…");
+
+
+// ex2. ----------------------------------
+
+//prints out 1.  
+console.log("Started request…");
+
+const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+fetchPromise2.then((response) => {
+  const jsonPromise = response.json();
+  //prints out 3.
+  console.log("request finished, now getting json()");
+  jsonPromise.then((data) => {
+    //prints out 4.
+    console.log(data[0].name);
+  });
+
+
+});
+//prints out 2.
+console.log("Waiting for the request to finish");
+
+// ex2. (improved version using promise chaining
+// -----------------------------------------------
+
+const fetchPromise3 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+// does the same exact thing as above
+// we now are using arrow functions 
+
+// prints out 1.
+console.log("Started request…");
+
+fetchPromise3
+  .then((response) => {
+    response.json();
+    //prints out 3.
+    console.log("request finished, now getting json()");
+ })
+  .then((data) => {
+    //prints out 4.
+    console.log(data[0].name);
+  });
+
+// one drawback of the improved version,
+// is it can be harder to debug.
+
+//prints out 2.
+console.log("waiting for a response"); 
+
